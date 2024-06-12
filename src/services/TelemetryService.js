83a65a2a947a36c7fba5c7cf74616e794200a1ae
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-06-12 09:04:37 -0300

    fix: telemetry caching

--- src/services/TelemetryService.js
+++ src/services/TelemetryService.js
@@ -7,21 +7,34 @@ const getLatestSensorData = async () => {
     "https://azr-iot-data-consumption.azurewebsites.net/api/most_recent_consumption"
   );
   const createdAt = new Date(response.data.mostRecentData.EnqueuedTimeUtc);
+  const timestamp = new Date();
+
+  if (response.status !== 200) {
+    throw new Error("Failed to get sensor data");
+  }
 
   return {
     createdAt,
+    timestamp,
     ...response.data.mostRecentData.Body,
   };
 };
 
+const getNextMeasurementTime = (lastMeasurement) => {
+  if (!lastMeasurement) {
+    return new Date();
+  }
+
+  return new Date(lastMeasurement.getTime() + 10 * 1000);
+};
+
 const getSensorData = async () => {
-  const lastMeasurement = sensorMeasurements?.timestamp || 0;
-  const nextMeasurement = lastMeasurement + 10000;
-  const isTimeToGetNewData = new Date() > nextMeasurement;
+  const lastMeasurement = sensorMeasurements?.timestamp;
+  const nextMeasurement = getNextMeasurementTime(lastMeasurement);
+  const isTimeToGetNewData = new Date() > new Date(nextMeasurement);
 
   if (!sensorMeasurements || isTimeToGetNewData) {
     sensorMeasurements = await getLatestSensorData();
-    sensorMeasurements.timestamp = new Date();
   }
 
   return sensorMeasurements;
