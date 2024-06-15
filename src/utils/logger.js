c8e5bceedb95a4d33810298cece604b8f0fe2522
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-06-15 08:47:23 -0300

    feat: add timestamp to log

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,19 +1,24 @@
 const winston = require("winston");
+const { combine, timestamp, prettyPrint, printf } = winston.format;
 const { LOG_LEVEL } = require("./environment");
 
 const logger = winston.createLogger({
   level: LOG_LEVEL,
-  format: winston.format.json(),
+  format: combine(timestamp(), prettyPrint()),
   transports: [
     new winston.transports.File({ filename: "error.log", level: "error" }),
     new winston.transports.File({ filename: "combined.log" }),
   ],
 });
 
+const myFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} [${level}]: ${message}`;
+});
+
 if (process.env.NODE_ENV !== "production") {
   logger.add(
     new winston.transports.Console({
-      format: winston.format.simple(),
+      format: myFormat,
     })
   );
 }
