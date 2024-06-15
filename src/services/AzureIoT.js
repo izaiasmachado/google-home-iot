908afabbdd9c64539c9836e088d2e544e5ab366a
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-06-14 21:53:36 -0300

    refactor: send messages from cloud to device

--- src/services/AzureIoT.js
+++ src/services/AzureIoT.js
@@ -3,20 +3,36 @@ const connectionString = process.env.AZURE_IOT_HUB_CONNECTION_STRING;
 const { Client } = require("azure-iothub");
 const { Message } = require("azure-iot-common");
 
-const client = Client.fromConnectionString(connectionString);
+class AzureIoTClient {
+  constructor(connectionString) {
+    this.connectionString = connectionString;
+    this.listeners = [];
+    this.client = Client.fromConnectionString(connectionString);
+    this.init();
+  }
 
-function sendMessage(deviceId, rawMessage) {
-  return new Promise((resolve, reject) => {
-    client.open((err) => {
-      if (err) {
-        console.log("Failed to open the connection to Azure IoT Hub");
-        return reject(err);
-      }
+  async init() {
+    await this.open();
+  }
 
-      console.log("Connection to Azure IoT Hub opened");
+  async open() {
+    return new Promise((resolve, reject) => {
+      this.client.open((err) => {
+        if (err) {
+          console.log("Failed to open the connection to Azure IoT Hub");
+          return reject(err);
+        }
 
+        console.log("Connection to Azure IoT Hub opened");
+        resolve();
+      });
+    });
+  }
+
+  async sendMessage(deviceId, rawMessage) {
+    return new Promise((resolve, reject) => {
       const message = new Message(rawMessage);
-      client.send(deviceId, message, (err) => {
+      this.client.send(deviceId, message, (err) => {
         if (err) {
           console.log(`Failed to send message to ${deviceId}`);
           return reject(err);
@@ -25,7 +41,7 @@ function sendMessage(deviceId, rawMessage) {
         resolve();
       });
     });
-  });
+  }
 }
 
-module.exports = { sendMessage };
+module.exports = new AzureIoTClient(connectionString);
